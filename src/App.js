91bfd0d0c188828c8257aaf9c1ad4e45b58a32e9
Jean Vera <jpierre22391@hotmail.com>
2024-10-29 04:38:08 -0300

    structure changes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from './Components/Header/Index';
-import Quotation from './Components/Quotation/Index';
 import MarketAnalysis from './Components/MarketAnalysis';
 import Footer from './Components/Footer/Index';
 /* import Home from './Components/Home/Index'; */
+
 // Components
-import Header from './Components/Header/Index';
 import Steps from './Components/Steps/Index';
 
 const App = () => {
